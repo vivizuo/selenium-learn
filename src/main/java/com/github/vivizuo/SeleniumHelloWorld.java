6be57053ca277616a8ed166cbcdf5252a75eb56e
wangwei <wangwei@quicloud.cn>
2016-06-14 15:37:32 +0800

    增加淘宝登录模拟

--- src/main/java/com/github/vivizuo/SeleniumHelloWorld.java
+++ src/main/java/com/github/vivizuo/SeleniumHelloWorld.java
@@ -1,28 +1,26 @@
 package com.github.vivizuo;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumHelloWorld {
 
     public static void main(String[] args) {
         //如果火狐浏览器没有默认安装在C盘，需要制定其路径
         //System.setProperty("webdriver.firefox.bin", "D:/Program Files/Mozilla firefox/firefox.exe");
-        System.setProperty("webdriver.chrome.driver", "E:/web/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/Users/wangwei/tools/Selenium/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.baidu.com/");
 
         driver.manage().window().maximize();
 
+        /*
         WebElement txtbox = driver.findElement(By.name("wd"));
         txtbox.sendKeys("Glen");
 
         WebElement btn = driver.findElement(By.id("su"));
         btn.click();
 
-        driver.close();
+        driver.close();*/
     }
 }
---
+++ src/main/java/com/github/vivizuo/TaobaoLogin.java
@@ -0,0 +1,44 @@
+package com.github.vivizuo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class TaobaoLogin {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/wangwei/tools/Selenium/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://login.taobao.com/");
+        driver.manage().window().maximize();
+        WebElement loginBox = driver.findElement(By.id("J_LoginBox"));
+        String classes = loginBox.getAttribute("class");
+        System.out.println("classes: " + classes);
+        if (classes.contains("module-static")) {
+            System.out.println("标准登录");
+        } else if (classes.contains("module-quick")) {
+            System.out.println("快速登录");
+            WebElement static2Quick = driver.findElement(By.className("login-switch"));
+            static2Quick.click();
+        }
+        WebElement usernameText = driver.findElement(By.name("TPL_username"));
+        usernameText.sendKeys("123");
+        WebElement passwordText = driver.findElement(By.name("TPL_password"));
+        passwordText.sendKeys("123");
+        WebElement loginButton = driver.findElement(By.id("J_SubmitStatic"));
+        loginButton.click();
+        System.out.println("cookies: " + driver.manage().getCookies());
+        driver.get("https://www.taobao.com/go/act/sale/keyword-dictionary.php");
+
+        List<WebElement> items = driver.findElement(By.id("Firstcoke"))
+                .findElement(By.tagName("ul"))
+                .findElements(By.tagName("li"));
+        for (WebElement item : items) {
+            item.findElement(By.tagName("a")).click();
+        }
+        //driver.close();
+    }
+}
